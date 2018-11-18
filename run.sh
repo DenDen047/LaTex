394b45fd4f2f620c98828145b893abc74e2f10fd
DenDen047 <sh.mn.nat@gmail.com>
2018-11-19 07:48:21 +0900

    update

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 f=$(basename $1)
 FILE=$(echo $f | sed 's/\.[^\.]*$//')   # 拡張子なしのファイル名を取得
 DIR=$(dirname $1)
+STYLE_DIR=${PWD}
 CMD="uplatex ${FILE}.tex && dvipdfmx ${FILE}.dvi"
 
 docker run -it --rm \
     -v ${DIR}:/root \
+    -v ${STYLE_DIR}/styles:/styles \
     -w /root \
     denden047/latex \
-    /bin/bash -c "${CMD}"
+    /bin/bash #-c "${CMD}"
