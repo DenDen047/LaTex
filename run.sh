be839409dd58b1de9ae8ad4202a50b566b16611b
Unknown <sh.mn.nat@gmail.com>
2018-11-18 11:16:35 +0900

    コメントを追加

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 f=$(basename $1)
-FILE=$(echo $f | sed 's/\.[^\.]*$//')
+FILE=$(echo $f | sed 's/\.[^\.]*$//')   # 拡張子なしのファイル名を取得
 DIR=$(dirname $1)
 CMD="uplatex ${FILE}.tex && dvipdfmx ${FILE}.dvi"
 
