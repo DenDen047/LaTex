12f54ead9149baf38450f87b8ce163ef1f22af51
DenDen047 <sh.mn.nat@gmail.com>
2021-01-10 21:39:38 +0900

    update

--- run.sh
+++ run.sh
@@ -4,11 +4,17 @@ f=$(basename $1)
 FILE=$(echo $f | sed 's/\.[^\.]*$//')   # 拡張子なしのファイル名を取得
 DIR=$(dirname $1)
 STYLE_DIR=${PWD}
-CMD="uplatex ${FILE}.tex && dvipdfmx ${FILE}.dvi"
+CMD="\
+    platex ${FILE}.tex && \
+    pbibtex ${FILE} && \
+    platex ${FILE}.tex && \
+    platex ${FILE}.tex && \
+    dvipdfmx ${FILE} \
+"
 
+docker build -q -t denden047/latex ./docker && \
 docker run -it --rm \
-    -v ${DIR}:/root \
-    -v ${STYLE_DIR}/styles:/styles \
-    -w /root \
+    -v ${DIR}:/texsrc \
+    -w /texsrc \
     denden047/latex \
     /bin/bash -c "${CMD}"
